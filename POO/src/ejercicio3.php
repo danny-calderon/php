9ae48a5760daf703a3d7977bb4ce2cc0e0595023
Danny Calderón <calderondanny2121@gmail.com>
2025-01-24 00:20:38 +0100

    añadimos el ejercicio 3

--- POO/src/ejercicio3.php
+++ POO/src/ejercicio3.php
@@ -1,2 +1,42 @@
 <?php 
+class Articulo {
+    //propiedades
+    public $nombre;
+    public $material;
+    public $precio;
+
+    //getters y setters
+        public function setnombre ($nombre){
+            $this->nombre = $nombre;
+        }
+        public function setmaterial ($material){
+            $this->material = $material;
+        }
+        public function setprecio ($precio){
+            $this->precio = $precio;
+        }
+        public function getnombre(){
+            return $this->nombre;
+        }
+        public function getmaterial(){
+            return $this->material;
+        }
+        public function getprecio(){
+            return $this->precio;
+        }
+    //metodo descuento
+    public function descuento($descuento)  {
+        
+        $decuento = $this->getprecio() * ($descuento/100);
+        $resultado = $this->getprecio() - $decuento;
+        echo "el precio con el descuento aplicado es: ". $resultado . "</br>";
+    }
+}
+//prueba
+$articulo = new Articulo;
+$articulo->setnombre("PC");
+$articulo->setmaterial("aluminio");
+$articulo->setprecio(100);
+$articulo->descuento(20);
+
 ?>
